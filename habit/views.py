11012ab945f4c1717e61f2a662d492d27258b207
elena_pantsurkina <pantsurkina.e@bk.ru>
2025-01-12 16:18:58 +0700

    созданы  для приложения habit сериализаторы, вьюшки и урлы

--- habit/views.py
+++ habit/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from habit.models import Habit
+from habit.serializers import HabitSerializer
 
-# Create your views here.
+
+class HabitViewSet(ModelViewSet):
+    """Вьюсет для модели Привычка."""
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
