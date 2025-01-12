d4fe0fb9962426be83484dba59f5d6ebbf849928
elena_pantsurkina <pantsurkina.e@bk.ru>
2025-01-12 17:17:50 +0700

    созданы  для приложения users сериализаторы, вьюшки и урлы, авторизация

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-
+from datetime import timedelta
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -26,6 +26,9 @@ INSTALLED_APPS = [
     "rest_framework",
     "users",
     "habit",
+    "django_filters",
+    "rest_framework_simplejwt",
+
 ]
 
 MIDDLEWARE = [
@@ -57,6 +60,16 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "config.wsgi.application"
+
+REST_FRAMEWORK = {
+    "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+}
 
 DATABASES = {
     "default": {
@@ -106,3 +119,8 @@ MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 AUTH_USER_MODEL = "users.User"
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+}
