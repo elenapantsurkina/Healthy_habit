b40d98168e861c93067a5de98ef7382cc444336c
elena_pantsurkina <pantsurkina.e@bk.ru>
2025-01-12 18:34:43 +0700

    реализована привязка пользователя к привычке

--- habit/views.py
+++ habit/views.py
@@ -1,9 +1,24 @@
 from rest_framework.viewsets import ModelViewSet
 from habit.models import Habit
 from habit.serializers import HabitSerializer
+from users.permissions import IsOwner
 
 
 class HabitViewSet(ModelViewSet):
     """Вьюсет для модели Привычка."""
     queryset = Habit.objects.all()
     serializer_class = HabitSerializer
+
+    """Метод для управления созданием объекта и автомат привязки создаваемого объекта к авторизованному пользователю."""
+    def perform_create(self, serializer):
+        habit = serializer.save()
+        habit.owner = self.request.user
+        habit.save()
+
+    def get_permissions(self):
+        """Метод определения действий в зависимости является ли пользователь владельцем."""
+        if self.action in ["update", "retrieve"]:
+            self.permission_classes = (IsOwner,)
+        elif self.action == "destroy":
+            self.permission_classes = (IsOwner,)
+        return super().get_permissions()
