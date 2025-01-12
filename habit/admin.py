98ff2524cbe9ff61ab414f123a78d93086ccc7be
elena_pantsurkina <pantsurkina.e@bk.ru>
2025-01-12 19:14:53 +0700

    настроена админка

--- habit/admin.py
+++ habit/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from habit.models import Habit
+
+
+@admin.register(Habit)
+class HabitAdmin(admin.ModelAdmin):
+    list_display = ("id", "habit", "action", "pleasant_habit", "related_habit", "award", "publicity",)
+    list_filter = ("publicity",)
+    search_fields = ("habit",)
+    
