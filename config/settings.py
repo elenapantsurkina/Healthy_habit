211e1088b0b59eb33b462427d8750de7de315108
elena_pantsurkina <pantsurkina.e@bk.ru>
2025-01-19 15:40:02 +0700

    доработаны настройки

--- config/settings.py
+++ config/settings.py
@@ -146,3 +146,11 @@ CELERY_TASK_TRACK_STARTED = True
 # Максимальное время на выполнение задачи
 CELERY_TASK_TIME_LIMIT = 30 * 60
 
+CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
+
+CELERY_BEAT_SCHEDULE = {
+    "send_information_telegram": {
+        "task": "habit.tasks.send_information_telegram",
+        "schedule": timedelta(days=1),
+    },
+}
